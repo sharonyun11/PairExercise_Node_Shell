f7f40c14c49a4abbcef5804e41dcf41886092970
Sharon Yun <sharonyun11@gmail.com>
2018-04-24 12:54:31 -0400

    3. Modules: Completed

--- bash.js
+++ bash.js
@@ -1,15 +1,7 @@
+const exportsFromPWD = require('./pwd');
+exportsFromPWD();
+
+const exportsFromLS = require('./ls');
+exportsFromLS();
+
 process.stdout.write('prompt > ');
-
-process.stdin.on('data', (data) => {
-  const cmd = data.toString().trim();
-
-  if (cmd === 'pwd') {
-    process.stdout.write(process.cwd());
-  }
-  else {
-    process.stdout.write('You typed: ' + cmd);
-  }
-  process.stdout.write('\nprompt > ');
-
-
-})
